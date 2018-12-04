3acbaed16f419a501f5acb966a0a992bae369b92
Morgan Grobin <mogrobin@microsoft.com>
2018-12-03 17:49:01 -0800

    Day 1 Part 1 Solution

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+            bool part1 = true; // Set True to run Part 1
+            bool part2 = false; // Set True to run Part 2
+
+            if (part1) {
+                string path = @"C:\Users\mogrobin\source\repos\AdventOfCode\AdventOfCode\Day1Input.txt";
+                Int32 partOneAnswer = PartOne(path);
+                Debug.WriteLine(partOneAnswer);
+            }
+
+            if (part2)
+            {
+
+            }
+
+            
+
+        }
+
+        static Int32 PartOne(string PathToFile)
+        {
+            int sum = 0;
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(PathToFile);
+            while ((line = file.ReadLine()) != null)
+            {
+                sum += Convert.ToInt32(line);
+            }
+
+
+            return sum;
+        }
+    }
+}
